de484a8206e73a4f76207807fb9db659b9626f8a
Ligia <ligiaarevalo251@gmail.com>
2026-06-25 14:03:52 -0400

    added get add and get post

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,27 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get('/game', async (req, res) => {
+  res.json({});
+
+})
+
+app.post("/games", (req, res) => {
+ // const { roomCode } = req.body;
+
+  res.json({
+    roomCode: "PLUM43",
+    letter: "B",
+    topic: "Animals"
+  });
+});
+
+app.post("/answers", (req, res) => {
+  res.json({
+    accepted: true
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
